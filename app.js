7914fe04b8d3aaf4a99bd0e59b63dd88ee97a6bc
Andy Glover <andrew.l.glover@gmail.com>
2024-05-23 20:34:53 -0700

    Fix duplicate declarations of readUsersData and writeUsersData in app.js

--- app.js
+++ app.js
@@ -14,22 +14,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-const usersFilePath = path.join(__dirname, "data", "users.json");
-
-// Read users data from the JSON file
-const readUsersData = () => {
-  if (fs.existsSync(usersFilePath)) {
-    const data = fs.readFileSync(usersFilePath, "utf8");
-    return JSON.parse(data);
-  }
-  return {};
-};
-
-// Write users data to the JSON file
-const writeUsersData = (data) => {
-  fs.writeFileSync(usersFilePath, JSON.stringify(data, null, 2), "utf8");
-};
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -68,14 +52,14 @@ app.get("/api/user/:username/focus", (req, res) => {
   res.json(users[username]);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.post("/api/focus-area", (req, res) => {
   const { name, description } = req.body;
   addFocusArea({ name, description });
   res
     .status(201)
     .json({ message: "Focus area added", focusArea: { name, description } });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
